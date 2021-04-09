39601c04ff148d673e5c80cf18af2b5994dbeaaf
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-04-09 11:52:09 +0300

    bonus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
-        int income = 1100;
+        int cash = 100;
+        int arrive = 1100;
         int bonus = 0;
-        if (income > 1000) {
-            bonus = income / 100;
+        if (arrive > 1000) {
+            bonus = arrive / 100;
         }
-        balance = balance + income + bonus;
+        cash = cash + arrive + bonus;
         System.out.println("Бонус: " + bonus);
-        System.out.println("Общая сумма: " + balance);
+        System.out.println("Общая сумма: " + cash);
     }
 }
